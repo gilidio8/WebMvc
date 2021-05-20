f5751c5f54c3045e6ed9e1e56b68e9415bc73ff0
Gabriel Ilídio Souza De Menezes <57880411+gilidio8@users.noreply.github.com>
2021-05-20 11:22:16 -0300

    barra de departamento na criação de vendedor

---
+++ WebApplication1/Services/DepartmentService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebMvc.Models;
+
+namespace WebMvc.Services
+{
+    public class DepartmentService
+    {
+        private readonly WebMvcContext _context;
+
+        public DepartmentService(WebMvcContext context)
+        {
+            _context = context;
+        }
+         public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+
+    }
+}
